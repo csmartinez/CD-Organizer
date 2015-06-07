4f6bff15e9d77f77b82ba19c09e2a0f2e0269afc
Carli Martinez <carlimartinez@Carlis-iMac.local>
2015-06-07 00:04:05 -0700

    clean up code, fix and style search results page

--- lib/cd_organizer.rb
+++ lib/cd_organizer.rb
@@ -1,36 +1,36 @@
 class CD
-attr_reader(:album, :artist)
-@@cddirectory = []
-  define_method(:initialize) do |attributes|
-    @album = attributes.fetch(:album)
-    @artist = attributes.fetch(:artist)
-    @id = @@cddirectory.length().+(1)
-  end
-
-  define_singleton_method(:all) do
-    @@cddirectory
-  end
-
-  define_method(:save) do
-    @@cddirectory.push(self)
-    self
-  end
-
-  define_singleton_method(:clear) do
-    @@cddirectory = []
-  end
-
-  define_method(:id) do
-    @id
-  end
-
-  define_singleton_method(:find) do |id|
-    found_cd = nil
-    @@cddirectory.each() do |cd|
-      if cd.id().eql?(id.to_i())
-        found_cd = cd
-      end
+  attr_reader(:album, :artist)
+  @@cddirectory = []
+    define_method(:initialize) do |attributes|
+      @album = attributes.fetch(:album)
+      @artist = attributes.fetch(:artist)
+      @id = @@cddirectory.length().+(1)
     end
-    found_cd
-  end
+
+    define_singleton_method(:all) do
+      @@cddirectory
+    end
+
+    define_method(:save) do
+      @@cddirectory.push(self)
+      self
+    end
+
+    define_singleton_method(:clear) do
+      @@cddirectory = []
+    end
+
+    define_method(:id) do
+      @id
+    end
+
+    define_singleton_method(:find) do |id|
+      found_cd = nil
+      @@cddirectory.each() do |cd|
+        if cd.id().eql?(id.to_i())
+          found_cd = cd
+        end
+      end
+      found_cd
+    end
 end
